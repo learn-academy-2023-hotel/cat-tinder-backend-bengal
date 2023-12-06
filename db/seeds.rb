61ba3f310e0be559221670085e5099a71982930d
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-12-06 15:21:18 -0800

    backend structure complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+cats = [
+    {
+        name: 'Blackwell',
+        age: 4,
+        enjoy: 'long walks on the beach',
+        image: 'https://twitter.com/susantananda3/status/1330866962675625988/photo/1'
+    },
+    {
+        name: 'Ash',
+        age: 6,
+        enjoy: 'eating catnip',
+        image: 'https://images.unsplash.com/photo-1513617753818-29509cdfdd9d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Badger',
+        age: 5,
+        enjoy: 'a nice long nap',
+        image: 'https://as2.ftcdn.net/v2/jpg/05/87/92/55/1000_F_587925530_VUYOLH6hI9RALjteSJo4T7gqYgHft9C2.jpg'
+    }
+]
+
+cats.each do |cat|
+    Cat.create cat
+    puts "creating cat #{cat}"
+end
