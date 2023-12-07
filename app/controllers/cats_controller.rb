01a9df4222e0411f86a8d3e7c3d31985b4ce87a2
anthonyholt <ant.scott227@gmail.com>
2023-12-06 16:34:30 -0800

    halfway point of api

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,22 @@
 class CatsController < ApplicationController
+    def index
+        cats = Cat.all
+        render json: cats 
+    end 
+
+    def create
+        cat = Cat.create(cat_params)
+        render json: cat
+    end 
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :age, :enjoy, :image)
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
 end
